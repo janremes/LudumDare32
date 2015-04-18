91673eabaed5c5f7a0658681f59ecd19a92be01a
Jan Remes <remeja@gmail.com>
2015-04-18 22:44:05 +0200

    added hiding initial values

--- js/Init.js
+++ js/Init.js
@@ -21,9 +21,10 @@ function InitGame()
     var elm = document.getElementById('svg-map').contentDocument;
 
     var svgMenu = Snap('#svg-nav');
+    var svgMap = Snap('#svg-map');
 
     var mapsvgdoc = null;
- 
+
     try {
         mapsvgdoc = elm.contentDocument;
     }
@@ -41,10 +42,27 @@ function InitGame()
         countryIds.push(id);
         var element = elm.getElementById(id);
 
+
+
         if (!element)
         {
             throw new Error("country with id " + id + " not found");
         }
+
+        var modifNames = ['tv', 'radio', 'noviny', 'net'];
+
+        for (var j = 0; j < modifNames.length; j++) {
+            var tvId = id + "_" + modifNames[j];
+            var svgTv = svgMap.select("#" + tvId);
+
+            if (!svgTv) {
+                console.log('missing:' + tvId);
+            } else {
+
+                svgTv.attr({display: 'none'});
+            }
+        }
+
 
         var newCountry = new Country(element);
         var newManager = new CountryManager(newCountry, element);
@@ -54,24 +72,24 @@ function InitGame()
         element.country = newCountry;
 
     }
-    
+
     countryData = [
-      //1 
-      { neighbours: [2,6],          neighboursPlayer : true,  popSize : new PopVector(15, 10), popularity : new PopVector(0.10,0.10) },
-      { neighbours: [1,3,6,7],      neighboursPlayer : true,  popSize : new PopVector( 5,  4), popularity : new PopVector(0.20,0.08) },
-      { neighbours: [2,8,7],        neighboursPlayer : true,  popSize : new PopVector( 4,  7), popularity : new PopVector(0.05,0.23) },
-      //4
-      { neighbours: [5,6],          neighboursPlayer : false, popSize : new PopVector( 3,  7), popularity : new PopVector(0.60,0.55) },
-      { neighbours: [4,6],          neighboursPlayer : false, popSize : new PopVector( 5,  2), popularity : new PopVector(0.40,0.55) },
-      { neighbours: [4,5,1,2,7],    neighboursPlayer : false, popSize : new PopVector(25, 16), popularity : new PopVector(0.50,0.45) },
-      //7
-      { neighbours: [6,2,3,8,9],    neighboursPlayer : false, popSize : new PopVector(12,  3), popularity : new PopVector(0.40,0.30) },
-      { neighbours: [7,3],          neighboursPlayer : false, popSize : new PopVector( 2,  9), popularity : new PopVector(0.20,0.60) },
-      { neighbours: [7],            neighboursPlayer : false, popSize : new PopVector(14,  2), popularity : new PopVector(0.60,0.50) }
+        //1 
+        {neighbours: [2, 6], neighboursPlayer: true, popSize: new PopVector(15, 10), popularity: new PopVector(0.10, 0.10)},
+        {neighbours: [1, 3, 6, 7], neighboursPlayer: true, popSize: new PopVector(5, 4), popularity: new PopVector(0.20, 0.08)},
+        {neighbours: [2, 8, 7], neighboursPlayer: true, popSize: new PopVector(4, 7), popularity: new PopVector(0.05, 0.23)},
+        //4
+        {neighbours: [5, 6], neighboursPlayer: false, popSize: new PopVector(3, 7), popularity: new PopVector(0.60, 0.55)},
+        {neighbours: [4, 6], neighboursPlayer: false, popSize: new PopVector(5, 2), popularity: new PopVector(0.40, 0.55)},
+        {neighbours: [4, 5, 1, 2, 7], neighboursPlayer: false, popSize: new PopVector(25, 16), popularity: new PopVector(0.50, 0.45)},
+        //7
+        {neighbours: [6, 2, 3, 8, 9], neighboursPlayer: false, popSize: new PopVector(12, 3), popularity: new PopVector(0.40, 0.30)},
+        {neighbours: [7, 3], neighboursPlayer: false, popSize: new PopVector(2, 9), popularity: new PopVector(0.20, 0.60)},
+        {neighbours: [7], neighboursPlayer: false, popSize: new PopVector(14, 2), popularity: new PopVector(0.60, 0.50)}
     ];
     for (var i = 0; i < countryData.length; i++) {
         countries[i].neighboursPlayer = countryData[i].neighboursPlayer;
-        for(var j = 0; j < countryData[i].neighbours.length; j++)
+        for (var j = 0; j < countryData[i].neighbours.length; j++)
         {
             countries[i].neighbouringCountries.push[countries[j]];
         }
@@ -84,20 +102,23 @@ function InitGame()
     managers.forEach(function (manager) {
         manager.svgElement.addEventListener("mousedown", function () {
             manager.onClick();
-            updateMenu(manager.country,manager.svgElement);
+            updateMenu(manager.country, manager.svgElement);
         });
     });
 
 
     function updateMenu(country, element) {
-        
+
         console.log('updating menu' + country);
         $(svgMenu.select('#popularity-old tspan').node).text(Math.round(country.popularity.old * 100) + '%');
         $(svgMenu.select('#popularity-young tspan').node).text(Math.round(country.popularity.young * 100) + '%');
-        
-        svgMenu.select('#pomer_happy_mlady').animate({width: country.popularity.young*193}, 500);
-        svgMenu.select('#pomer_happy_stary').animate({width:  country.popularity.old*193}, 500);
+
+        svgMenu.select('#pomer_happy_mlady').animate({width: country.popularity.young * 193}, 500);
+        svgMenu.select('#pomer_happy_stary').animate({width: country.popularity.old * 193}, 500);
     }
+
+
+
 
     gameState = new GameState(countries);
 
